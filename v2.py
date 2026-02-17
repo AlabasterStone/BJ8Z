81d44134b843304522e0696b10c561c3f4576a99
int0x19 <thewavylines@outlook.com>
2026-02-17 09:00:27 +0800

    Update v2.py

--- v2.py
+++ v2.py
@@ -1,19 +1,63 @@
 import requests
 import logging
-params = {
-    "termId": "afd4339f93e84dcf8cc64bc616a17bf2",
-    "courseId": "134a8f908a1946b0913ee1fdf85f3ecc",
-    "versionNum": 3,
-    "force": "force"
-}
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0",
-    "Cookie": ""
-}
-url = "http://bjsdbzx.bjbzszxy.cn/xsxk/studentElective/redisStudentSelectCourse.do"
-
-res = requests.post(url, data=params, headers=headers)
+import threading
+from configs_v2 import *
+import time
+from datetime import datetime
 logging.basicConfig(level=logging.INFO)
-logging.info(f"{res.status_code}: {res.text}")
-print(res.text)
+
+targetTime = "2026-02-17 08:45:45"
+timeFormat = "%Y-%m-%d %H:%M:%S"
+
+def getTime() -> str:
+    return time.strftime(timeFormat, time.localtime())
+
+
+def isOnTime(currentTime: str):
+    return currentTime == targetTime
+
+def isPastTime(currentTime: str, targetTime: str) -> bool:
+    current = datetime.strptime(currentTime, timeFormat)
+    target = datetime.strptime(targetTime, timeFormat)
+    return current > target
+
+def getRemainingTime(time_str1: str, time_str2: str, time_format: str) -> str:
+    dt1 = datetime.strptime(time_str1, time_format)
+    dt2 = datetime.strptime(time_str2, time_format)
+    delta = dt2 - dt1
+    total_seconds = abs(delta.total_seconds())
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) // 60)
+    seconds = int(total_seconds % 60)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+def sendRequest(params: dict[str, str|int]) -> None:
+    url = "https://bjsdbzx.bjbzszxy.cn/xsxk/studentElective/redisStudentSelectCourse.do"
+    headers = {
+        "User-Agent": user_agent,
+        "Cookie": cookie
+    }
+    while True:
+        res = requests.post(url, data=params, headers=headers)
+        logging.info(f"Course ID: {params['courseId']} : {res.text}")
+
+while True:
+    if isPastTime(getTime(), targetTime):
+        break
+    else:
+        print(
+            f"\r{getRemainingTime(getTime(), targetTime, timeFormat)}", flush=True, end="")
+        continue
+
+for courseId in coursesId:
+    params = {
+        "termId": termId,
+        "courseId": courseId,
+        "versionNum": versionNum,
+        "force": force
+    }
+    threading.Thread(target=sendRequest, args=(params,)).start()
+
+
+
+
