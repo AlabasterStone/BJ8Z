5842a81d3b7933b4249e2ffcf5e20fddf7dab5a2
int0x19 <thewavylines@outlook.com>
2026-02-17 09:00:54 +0800

    Refactor configuration in configs_v2.py
    Removed course definitions and added new configuration variables.

---
+++ configs_v2.py
@@ -0,0 +1,8 @@
+termId = "afd4339f93e84dcf8cc64bc616a17bf2"
+versionNum = 3
+force = "force"
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
+cookie = r""
+
+coursesId = ["1","2","3","4"]
